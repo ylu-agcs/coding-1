1b9356721a05bdad3ce25eeedeb3a220b49c186e
ylu-agcs <85938247+ylu-agcs@users.noreply.github.com>
2021-06-16 10:49:46 +0200

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,9 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+Example:
+Input: ["abc", "abd", "abe"]
+Output: "ab"
+"""
+
+def longest_common_prefix(strs: List[str])
+    return
