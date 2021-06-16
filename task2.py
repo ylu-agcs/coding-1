38cff94d1a628b901884effffe1f089a1065d10a
ylu-agcs <85938247+ylu-agcs@users.noreply.github.com>
2021-06-16 10:31:07 +0200

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+"""
+Task: Implement function to generate a set of 2d data points that are uniformly distributed inside a circle (c, r), where c=(x,y) is the center, and r is the radius. Return the dataset as a pandas dataframe
+"""
+
+def random_points_in_circle(c: tuple, r: float):
+    return
